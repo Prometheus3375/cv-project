7aec71d76fb1ba53c94dfc91a33815f5ff4ebe97
Prometheus <prometheus3375@gmail.com>
2020-09-27 02:27:12 +0300

    Fix errors in time measuring
    Added module time_.py to convert seconds to proper h:m:s string and
    function decorator to measure time.

--- train_real_fixed.py
+++ train_real_fixed.py
@@ -1,7 +1,5 @@
 import argparse
 import os
-from datetime import time
-from timeit import default_timer as get_time
 
 import torch.nn as nn
 from tensorboardX import SummaryWriter
@@ -12,6 +10,7 @@ from data_loader import VideoData
 from functions import *
 from loss_functions import GANloss, alpha_gradient_loss, alpha_loss, compose_loss
 from networks import MultiscaleDiscriminator, ResnetConditionHR, conv_init
+from time_ import get_time, print_time_elapsed
 
 
 def collate_filter_none(batch):
@@ -19,6 +18,7 @@ def collate_filter_none(batch):
     return default_collate(batch)
 
 
+@print_time_elapsed
 def main():
     # CUDA
 
@@ -270,10 +270,4 @@ def main():
 
 
 if __name__ == '__main__':
-    start = get_time()
     main()
-    end = get_time()
-    seconds = int(start - end)
-
-    t = time(second = seconds)
-    print(f'Time elapsed: {t.hour}h {t.minute}m {t.second}s')
